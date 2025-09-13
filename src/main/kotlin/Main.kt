6aa18566403b3765425d305f8525756162c8ddfe
chuvikina <sasha.chuvakina@outlook.com>
2025-09-13 17:09:47 +0300

    Added some changes (menu, task2)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,54 @@
 import java.lang.StringBuilder
 
 fun main() {
+    while (true) {
+        println("0 выход")
+        println("1 задача")
+        println("2 задача")
+        print("Выберите пункт меню: ")
+        val menu = readln()
+        when (menu) {
+            "0" -> return
+            "1" -> task1()
+            else -> println("Неверный ввод")
+        }
+    }
+}
+fun task1() {
+    println("Данная программа преобразовывет строку с буквами. Если 1 буква повторяется несколько раз, то программа вывлдит букву и её количество")
+    println("Пожалуйста введите набор букв")
 
-    var testD = "AAADSSSRRTTHAAAA"
+    var input = readln()
+    if (!input.all { it.isLetter() }) {
+        println("Вы можете вводить только буквы!")
+        return
+    }
     var result = StringBuilder()
     var count = 1
-    println("Данная программа преобразовывет строку с буквами. Если 1 буква повторяется несколько раз, то программа вывлдит букву и её количество")
-    for (i in 1..testD.length-1) {
-        if (testD[i] == testD[i - 1]) {
+    for (i in 1..input.length - 1) {
+        if (input[i] == input[i - 1]) {
             count++
         } else {
-            result.append(testD[i - 1])
+            result.append(input[i - 1])
             if (count > 1) result.append(count)
             count = 1
         }
     }
-    result.append(testD.last())
+    result.append(input.last())
     if (count > 1) result.append(count)
     println(result.toString())
 }
+fun task2() {
+    println("Данная программа подсчитывает количество различных различных символов во введённой строке")
+    println("Пожалуйста введите набор букв")
+
+    var input = readln()
+    if (!input.all { it.isLetter() }) {
+        println("Вы можете вводить только буквы!")
+        return
+    }
+    var result: String = ""
+    var count = input
+        .groupingBy {  }
+}
+
